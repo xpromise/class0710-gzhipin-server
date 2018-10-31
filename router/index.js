7c9648f5a9fd74f5feec883b3448af478b26e812
xiongjian <webjsforyou@gmail.com>
2018-10-31 17:34:00 +0800

    实现登录和注册路由

--- router/index.js
+++ router/index.js
@@ -15,12 +15,50 @@ const router = new Router();
 router.use(express.urlencoded({extended: true}));
 
 //登录
-router.post('/login', (req, res) => {
-  res.send('login路由的响应111111111111111111');
+router.post('/login', async (req, res) => {
+  //1. 收集用户提交信息
+  const {username, password} = req.body;
+  //2. 判断用户输入是否合法
+  if (!username || !password) {
+    //说明有数据不合法
+    res.json({
+      "code": 2,
+      "msg": "用户输入不合法"
+    });
+    return;
+  }
+  //3. 去数据库中查找是否有指定用户和密码
+  try {
+    const data = await Users.findOne({username, password: md5(password)});
+  
+    if (data) {
+      //说明用户找到了，登录成功，返回成功的响应
+      res.json({
+        "code": 0,
+        "data": {
+          "_id": data.id,
+          "username": data.username,
+          "type": data.type
+        }
+      })
+    } else {
+      //说明用户名或密码错误，返回失败的响应
+      res.json({
+        "code": 1,
+        "msg": "用户名或密码错误"
+      })
+    }
+  } catch (e) {
+    res.json({
+      "code": 3,
+      "msg": "网络不稳定，请重新试试~"
+    })
+  }
+  
 })
 
 //注册
-router.post('/register', (req, res) => {
+router.post('/register',async (req, res) => {
   // 1. 收集用户提交信息
   const {username, password, type} = req.body;
   console.log(username, password, type);
@@ -34,45 +72,85 @@ router.post('/register', (req, res) => {
     return;
   }
   // 3. 去数据库查找用户是否存在
-  Users.findOne({username}, (err, data) => {
-    if (!err) {
-      //方法没有出错
+  /*Users.findOne({username})
+    .then(data => {
+      console.log(data);  //文档对象
       if (data) {
-        //找到了指定用户，用户名已存在
-        res.json({
+        //返回错误
+        return Promise.reject({
           "code": 1,
-          "msg": "此用户已存在"
+          "msg": "用户名已存在"
         });
       } else {
-        // 4. 将用户信息保存在数据库中
-        Users.create({username, password: md5(password), type}, (err, data) => {
-          if (!err) {
-            //注册成功
-            res.json({  //将js对象/数组转换成json字符串
-              code: 0,
-              data: {
-                _id: data.id,
-                username: data.username,
-                type: data.type
-              }
-            })
-          } else {
-            //方法出错了
-            res.json({
-              "code": 3,
-              "msg": "网络不稳定，请重新试试~"
-            });
-          }
-        })
+        console.log('1111111111');
+        return Users.create({username, password: md5(password), type})
       }
-    } else {
-      //方法出错了
+    })
+    .catch(err => {
+      console.log('第一个catch');
+      console.log(err);
+      if (!err.code) {
+        err = {
+          "code": 3,
+          "msg": "网络不稳定，请重新试试~"
+        }
+      }
+      //方法出错
+      return Promise.reject(err);
+    })
+    .then(data => {
+      //说明用户注册成功, 返回成功的响应
       res.json({
-        "code": 3,
-        "msg": "网络不稳定，请重新试试~"
+        code: 0,
+        data: {
+          _id: data.id,
+          username: data.username,
+          type: data.type
+        }
+      })
+    })
+    .catch(err => {
+      console.log('第二个catch');
+      console.log(err);
+      if (!err.code) {
+        err = {
+          "code": 3,
+          "msg": "网络不稳定，请重新试试~"
+        }
+      }
+      //返回失败的响应
+      res.json(err);
+    })*/
+  try {
+    const data = await Users.findOne({username});
+  
+    if (data) {
+      //返回错误
+      res.json({
+        "code": 1,
+        "msg": "用户名已存在"
       });
+    } else {
+      const data = await Users.create({username, password: md5(password), type});
+      //返回成功的响应
+      res.json({
+        code: 0,
+        data: {
+          _id: data.id,
+          username: data.username,
+          type: data.type
+        }
+      })
     }
-  })
+  } catch (e) {
+    //说明findOne / create方法出错了
+    //返回失败的响应
+    res.json({
+      "code": 3,
+      "msg": "网络不稳定，请重新试试~"
+    })
+  }
+  
 })
 
 //暴露出去
